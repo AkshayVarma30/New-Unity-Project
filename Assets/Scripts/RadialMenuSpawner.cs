5f5ce180108f7b36aece6fb2721babc10ebc9cdd
AkshayVarma30 <56615714+AkshayVarma30@users.noreply.github.com>
2019-10-16 23:26:58 +0530

    Revert "Revert "Added tower building UI""
    This reverts commit a8d3ad7a3844a4c848326ae1d1f03b01582f6a71.

---
+++ Assets/Scripts/RadialMenuSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialMenuSpawner : MonoBehaviour
+{
+    public static RadialMenuSpawner instance;
+    public RadialMenu menu;
+    public int selectedButtonIndex;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void spawnMenu(capsule obj)
+    {
+            RadialMenu newMenu = Instantiate(menu, transform);
+            newMenu.transform.position = Input.mousePosition;
+            newMenu.spawnButtons(obj);
+    }
+}
