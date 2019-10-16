a8d3ad7a3844a4c848326ae1d1f03b01582f6a71
AkshayVarma30 <56615714+AkshayVarma30@users.noreply.github.com>
2019-10-16 23:25:39 +0530

    Revert "Added tower building UI"
    This reverts commit 31fd8aed6e2d71a6f7ebe63eb91b5e748c59d90f.

--- Assets/Scripts/capsule.cs
+++ Assets/Scripts/capsule.cs
@@ -3,21 +3,11 @@ using UnityEngine;
 
 public class capsule : MonoBehaviour
 {
-    [System.Serializable]
-    public class towerButtons
-    {
-        public Color color;
-        public Sprite sprite;
-        public GameObject tower;
-    }
-    public towerButtons[] arrayOfButtons;
-
     public Color hoverColor;
     public Renderer rend;
-    public Vector3 positionOffset;
+    public GameObject towerPrefab;
     void Start()
     {
-       
         rend = gameObject.GetComponent<Renderer>();
     }
 
@@ -31,14 +21,18 @@ public class capsule : MonoBehaviour
         rend.material.color = Color.white;
     }
     private void OnMouseDown()
-    { 
-            RadialMenuSpawner.instance.spawnMenu(this);
+    {
+        towerBuildingAI();   
     }
-    public void towerBuildingAI(int i)
+    void towerBuildingAI()
     {
         Debug.Log("working!");
         Destroy(gameObject);
-        Instantiate(arrayOfButtons[i].tower, transform.position+positionOffset, transform.rotation);
+        Instantiate(towerPrefab, transform.position, transform.rotation);
     }
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
